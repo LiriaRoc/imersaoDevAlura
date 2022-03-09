359c7478dacd194b27961c4ca9aa70692d156e0a
LiriaRoc <101200319+LiriaRoc@users.noreply.github.com>
2022-03-09 12:06:26 -0300

    Add files via upload

---
+++ calculadora-de-m-dia/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/liriaroc/pen/MWOMVBy](https://codepen.io/liriaroc/pen/MWOMVBy).
+
+
---
+++ calculadora-de-m-dia/dist/script.js
@@ -0,0 +1,12 @@
+var nome = "Líria"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 8
+var notaDoQuartoBimestre = 7
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)   
+
+console.log ("Bem vindo, " + nome)
+console.log (notaFixada)
